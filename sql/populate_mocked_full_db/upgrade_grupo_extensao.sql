b0dbe66558cdc7559b8635a61c58a97d2b8657a8
brcls <petrick_barcelos@hotmail.com>
2025-11-17 16:06:45 -0300

    fix: update occupancy rate display and correct group activity IDs
    - Adjusted the occupancy rate display in the admin dashboard to cap at 100% for better user experience.
    - Updated group activity IDs in the CSV and SQL population scripts to ensure consistency across the database.
    - Modified the group descriptions and responsible CPF entries for accuracy in the extension groups data.

--- sql/populate_mocked_full_db/upgrade_grupo_extensao.sql
+++ sql/populate_mocked_full_db/upgrade_grupo_extensao.sql
@@ -1,4 +1,4 @@
-INSERT INTO GRUPO_EXTENSAO (NOME_GRUPO, DESCRICAO, CPF_RESPONSAVEL_INTERNO) VALUES ('Projeto Capoeira Angola', 'Projeto Capoeira Angola é um grupo de extensão promovido pelo CEFER.', '17017674144');
-INSERT INTO GRUPO_EXTENSAO (NOME_GRUPO, DESCRICAO, CPF_RESPONSAVEL_INTERNO) VALUES ('Equipe Kung Fu Garra de Águia', 'Equipe Kung Fu Garra de Águia é um grupo de extensão promovido pelo CEFER.', '48429995199');
-INSERT INTO GRUPO_EXTENSAO (NOME_GRUPO, DESCRICAO, CPF_RESPONSAVEL_INTERNO) VALUES ('Grupo de Karatê Shotokan', 'Grupo de Karatê Shotokan é um grupo de extensão promovido pelo CEFER.', '50157145644');
-INSERT INTO GRUPO_EXTENSAO (NOME_GRUPO, DESCRICAO, CPF_RESPONSAVEL_INTERNO) VALUES ('Grupo de Estudos Tai Chi Chuan', 'Grupo de Estudos Tai Chi Chuan é um grupo de extensão promovido pelo CEFER.', '73541738944');
+INSERT INTO GRUPO_EXTENSAO (NOME_GRUPO, DESCRICAO, CPF_RESPONSAVEL_INTERNO) VALUES ('Projeto Capoeira Angola', 'Projeto Capoeira Angola é um grupo de extensão promovido pelo CEFER.', '18624009977');
+INSERT INTO GRUPO_EXTENSAO (NOME_GRUPO, DESCRICAO, CPF_RESPONSAVEL_INTERNO) VALUES ('Grupo de Estudos Tai Chi Chuan', 'Grupo de Estudos Tai Chi Chuan é um grupo de extensão promovido pelo CEFER.', '22910299677');
+INSERT INTO GRUPO_EXTENSAO (NOME_GRUPO, DESCRICAO, CPF_RESPONSAVEL_INTERNO) VALUES ('Equipe Kung Fu Garra de Águia', 'Equipe Kung Fu Garra de Águia é um grupo de extensão promovido pelo CEFER.', '56356667799');
+INSERT INTO GRUPO_EXTENSAO (NOME_GRUPO, DESCRICAO, CPF_RESPONSAVEL_INTERNO) VALUES ('Grupo de Karatê Shotokan', 'Grupo de Karatê Shotokan é um grupo de extensão promovido pelo CEFER.', '40986040000');
