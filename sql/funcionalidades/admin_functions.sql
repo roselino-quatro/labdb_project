4a23202cb22d3847d39303e16830a1303cf49682
Gabriel Santos <gabriel.dossantos@usp.br>
2025-11-19 15:58:24 -0300

    adicionado procedures de adm (grupo de extensao, equipamento, instalacao e evento)

--- sql/funcionalidades/admin_functions.sql
+++ sql/funcionalidades/admin_functions.sql
@@ -124,3 +124,46 @@ BEGIN
     IF NOT FOUND THEN RAISE EXCEPTION 'Instalação não encontrada.'; END IF;
 END;
 $$;
+
+-- Evento
+-- CREATE
+CREATE OR REPLACE PROCEDURE criar_evento(
+    p_nome VARCHAR,
+    p_descricao TEXT,
+    p_id_reserva INT
+) LANGUAGE plpgsql AS $$
+BEGIN
+    -- Verifica se a reserva existe
+    IF NOT EXISTS (SELECT 1 FROM RESERVA WHERE ID_RESERVA = p_id_reserva) THEN
+        RAISE EXCEPTION 'Reserva % não encontrada. Crie a reserva antes do evento.', p_id_reserva;
+    END IF;
+
+    INSERT INTO EVENTO (NOME, DESCRICAO, ID_RESERVA)
+    VALUES (p_nome, p_descricao, p_id_reserva);
+END;
+$$;
+
+-- UPDATE
+CREATE OR REPLACE PROCEDURE atualizar_evento(
+    p_id_evento INT,
+    p_nome_novo VARCHAR,
+    p_descricao_nova TEXT
+) LANGUAGE plpgsql AS $$
+BEGIN
+    UPDATE EVENTO
+    SET NOME = p_nome_novo,
+        DESCRICAO = p_descricao_nova
+    WHERE ID_EVENTO = p_id_evento;
+
+    IF NOT FOUND THEN RAISE EXCEPTION 'Evento não encontrado.'; END IF;
+END;
+$$;
+
+-- DELETE
+CREATE OR REPLACE PROCEDURE deletar_evento(p_id_evento INT)
+LANGUAGE plpgsql AS $$
+BEGIN
+    DELETE FROM EVENTO WHERE ID_EVENTO = p_id_evento;
+    IF NOT FOUND THEN RAISE EXCEPTION 'Evento não encontrado.'; END IF;
+END;
+$$;
