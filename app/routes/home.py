aa850b67336bee3cd66659aaa3209145ce783c7d
roselino-quatro <lou.roselino@gmail.com>
2025-11-20 14:28:41 -0300

    fix: changing home redirect
    Home was redirecting to admin, admin was redirecting to home on not
    enough credentials so the whole application was looping on refresh

--- app/routes/home.py
+++ app/routes/home.py
@@ -6,7 +6,7 @@ home_blueprint = Blueprint("home", __name__)
 
 @home_blueprint.route("/", methods=["GET"])
 def index():
-    return redirect(url_for("admin.dashboard"))
+    return redirect(url_for("internal.dashboard"))
 
 
 home_blueprint.add_url_rule("/", endpoint="dashboard", view_func=index)
